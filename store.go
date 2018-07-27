49b26a4241b6ae9e164320a04bf3987d284871a1
Rasarts <rasart.pro@gmail.com>
2018-07-27 22:24:31 +0300

    Added Query method of Store type.

--- store.go
+++ store.go
@@ -1,9 +1,9 @@
 package storage
 
 import (
-	"log"
-
+	"context"
 	"google.golang.org/grpc"
+	"log"
 
 	dataBaseClient "github.com/dgraph-io/dgo"
 	dataBaseAPI "github.com/dgraph-io/dgo/protos/api"
@@ -12,14 +12,11 @@ import (
 // Store is a object with database resource
 type Store struct {
 	DatabaseGateway string
-	Client          *dataBaseClient.Dgraph
 }
 
 // New is a constructor for Store objects
 func New(databaseGateway string) *Store {
-	storage := &Store{
-		DatabaseGateway: databaseGateway}
-
+	storage := &Store{DatabaseGateway: databaseGateway}
 	return storage
 }
 
@@ -33,7 +30,20 @@ func (store *Store) PrepareDataBaseClient(databaseGateway string) (*dataBaseClie
 	baseClient := dataBaseAPI.NewDgraphClient(conn)
 	databaseGraph := dataBaseClient.NewDgraphClient(baseClient)
 
-	store.Client = databaseGraph
-
 	return databaseGraph, nil
+}
+
+func (store *Store) Query(request string) (response []byte, err error) {
+	client, err := store.PrepareDataBaseClient(store.DatabaseGateway)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction := client.NewTxn()
+	resp, err := transaction.Query(context.Background(), request)
+	if err != nil {
+		return response, err
+	}
+
+	return resp.GetJson(), nil
 }
